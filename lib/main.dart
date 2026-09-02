378199e08a9e7944bd31d705cc6bfaf3d0586ebd
Stepsinsider <119820978+StepsInsider@users.noreply.github.com>
2026-09-02 19:33:42 +0200

    fix: use stabilized Pincus workspace entrypoint

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
-import 'pincus_workspace.dart';
+import 'pincus_workspace_v2.dart';
 
 void main() {
-  runApp(const MaterialApp(debugShowCheckedModeBanner: false, home: PincusWorkspace()));
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: PincusWorkspace(),
+  ));
 }

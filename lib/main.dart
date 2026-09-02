ae35e15cbea4f8f3401df8721235ec35d24079b5
Stepsinsider <119820978+StepsInsider@users.noreply.github.com>
2026-09-02 19:28:19 +0200

    Fix app entrypoint export

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,5 @@
-import 'package:flutter/material.dart';
 import 'pincus_workspace.dart';
 
 void main() {
   runApp(const PincusApp());
-}
-
-class PincusApp extends StatelessWidget {
-  const PincusApp({super.key});
-  @override
-  Widget build(BuildContext context) => const PincusWorkspace();
 }
